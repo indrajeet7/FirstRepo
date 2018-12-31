39f498ad3d03c4f6adcaa88b8657e29436d91607
Indrajeet <indrajeetphutane@gmail.com>
2018-12-31 15:04:24 +0530

    correct the test script

--- test_ping.py
+++ test_ping.py
@@ -1,10 +1,15 @@
-from practice_multiping import *
+from ping import *
 
-ip_addr=input("Enter IP Address.......")
-iteration=int(input("Enter number of iterations....."))
+ip_addr='192.168.78.50'
+ping_count=10
 
 
 def test_ping_func():
-    assert ping_func(ip_addr,iteration)==True
+    average_rtt, loss = ping(ip_addr, ping_count)
+    status = False
 
+    # Fail condition
+    if loss < 30 and average_rtt <= 5:
+        status = True
 
+    assert status == True
